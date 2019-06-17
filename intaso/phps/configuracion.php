ea11ec1e114a674f0051cb7e143fe3c65e618f7c
dayvis80 <42790291+dayvis80@users.noreply.github.com>
2019-06-17 08:08:48 -0500

    Update configuracion.php

--- intaso/phps/configuracion.php
+++ intaso/phps/configuracion.php
@@ -3,7 +3,7 @@
 date_default_timezone_set("America/Lima");
 
 // Variables de Conexion Base de Datos
-$host = 'intaso2.sytes.net'; // 'intaso.sytes.net'; //'localhost';
+$host = 'intaso.sytes.net'; // 'intaso.sytes.net'; //'localhost';
 $puerto = '5432';
 $usuario = 'postgres';
 $password = 'intaso2018'; //'intaso2018';//'123456';
